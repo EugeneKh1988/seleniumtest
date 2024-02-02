4fb8063cf46664f2e61d42a84e39cf039297fc5d
Eugene <khiznyak.1988@gmail.com>
2024-02-02 18:53:41 +0200

    added debugger address

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+const Chrome = require('selenium-webdriver/chrome');
 
-(async function example() {
-  let driver = await new Builder().forBrowser(Browser.CHROME).build();
+async function start() {
+    const options = new Chrome.Options();
+  let driver = await new Builder()
+    .setChromeOptions(options.debuggerAddress("localhost:63814"))
+    .forBrowser(Browser.CHROME)
+    .build();
   try {
     await driver.get('https://www.google.com/ncr');
+    let ss = await driver.getCapabilities();
+    console.log(ss);
     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
     await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
   } finally {
     //await driver.quit();
   }
-})();
+}
+
+start();
